0d115036ea96a77031163f146f6c662638705448
mnb3916 <912987977@qq.com>
2017-04-24 11:05:54 +0800

    Initial Commit

---
+++ new score/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  new score
+//
+//  Created by s20151104708 on 17/4/24.
+//  Copyright © 2017年 s20151104708. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
