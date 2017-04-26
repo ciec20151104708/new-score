0531b27a6041ffa6c01816d825f170d9c71c8700
mnb3916 <912987977@qq.com>
2017-04-26 10:35:06 +0800

    score
    能够在正确的计算比分，输出获胜的是谁

--- new score/ViewController.swift
+++ new score/ViewController.swift
@@ -7,19 +7,116 @@
 //
 
 import UIKit
+var x : Int = 0
+var y : Int = 0
+var n : Int = 0
+var m : Int = 0
+var i : Int = 0
+var j : Int = 0
+var flagA : Int=0
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate{
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var pingpang: UIImageView!
+    @IBOutlet weak var Aimg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func Aput(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+        //初始化图片控制器
+        let picker = UIImagePickerController()
+        //设置代理
+        picker.delegate = self
+        //指定图片控制器类型
+        picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        //弹出控制器，显示界面
+        self.present(picker, animated: true, completion: {
+            () -> Void in
+        })
+     }else{
+    print("读取相册错误")
+      }
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        //查看info对象
+        print(info)
+        //显示的图片
+        let image:UIImage!
+        //获取选择的原图
+        image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        if(flagA==1){Aimg.image = image}
+        picker.dismiss(animated: true, completion: {
+            () -> Void in
+        })
+    }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var A: UITextField!
+    @IBOutlet weak var B: UITextField!
+    @IBOutlet weak var X: UITextField!
 
+    @IBOutlet weak var Y: UITextField!
+    @IBOutlet weak var Z: UITextField!
+    @IBOutlet weak var N: UITextField!
 
+    
+    @IBAction func start(_ sender: Any) {
+        x = 0
+        y = 0
+        A.text = "\(x)"
+        B.text = "\(y)"
+    }
+    @IBAction func Aadd(_ sender: UIButton) {
+        x = x + 1
+        A.text = "\(x)"
+        if x >= 10 && y >= 10{
+            if x >= y + 2{
+                X.text = "A选手获胜"
+                i = i + 1
+                n = n + 1
+            }
+        }else if  x > y && x == 11 {
+            X.text = "A选手获胜"
+            i = i + 1
+            n = n + 1
+            
+    }
+    }
+    @IBAction func Badd(_ sender: UIButton) {
+        y = y + 1
+        B.text = "\(y)"
+        if x >= 10 && y >= 10{
+            if y >= x + 2{
+                X.text = "B选手获胜"
+                j = j + 1
+                m = m + 1
+            }
+        }else if  y > x && y == 11 {
+            X.text = "B选手获胜"
+            j = j + 1
+            m = m + 1
+            
+        }
+    }
+    @IBAction func Aback(_ sender: UIButton) {
+        A.text = "\(x)"
+        x = x - 1
+        A.text = "\(x)"
+    }
+    @IBAction func Bback(_ sender: UIButton) {
+        B.text = "\(y)"
+        y = y - 1
+        B.text = "\(y)"
+    }
+    @IBAction func end(_ sender: Any) {
+        x = 0
+        y = 0
+        A.text = "\(x)"
+        B.text = "\(y)"
+        X.text = ""
 }
-
+}
