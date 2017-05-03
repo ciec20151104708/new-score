97d9a41cd2e247752a799a24ad8b9e729b3b1c2d
mnb3916 <912987977@qq.com>
2017-05-03 11:49:54 +0800

    score
    1、计算发球还有些Bug
    2、比分在一方获胜之后还可以继续加

--- new score/ViewController.swift
+++ new score/ViewController.swift
@@ -13,6 +13,8 @@ var n : Int = 0
 var m : Int = 0
 var i : Int = 0
 var j : Int = 0
+var c : Int = 0
+
 var flagA : Int=0
 var flagB : Int=0
 var dlg : Int = 0
@@ -87,6 +89,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
     @IBOutlet weak var Y: UITextField!
     @IBOutlet weak var Z: UITextField!
     @IBOutlet weak var N: UITextField!
+    @IBOutlet weak var M: UITextField!
 
     
     @IBAction func start(_ sender: Any) {
@@ -97,23 +100,41 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
     }
     @IBAction func Aadd(_ sender: UIButton) {
         x = x + 1
+        c = c + 1
         A.text = "\(x)"
+        if c<2{
+            M.text = "A发球"
+        }else if c>2{
+            c = 0
+            M.text = "B发球"
+        }
         if x >= 10 && y >= 10{
             if x >= y + 2{
                 X.text = "A选手获胜"
                 i = i + 1
                 n = n + 1
+                A.text = "\(11)"
                 
             }
         }else if  x > y && x == 11 {
+            
             X.text = "A选手获胜"
+            if X.text != "A选手获胜" {
+                x = 0
+            }
             i = i + 1
             n = n + 1
     }
+    
     }
     @IBAction func Badd(_ sender: UIButton) {
-        
         y = y + 1
+        if c<2{
+            M.text = "B发球"
+        }else if c>=2{
+            c = 0
+            M.text = "A发球"
+        }
         B.text = "\(y)"
         if x >= 10 && y >= 10{
             if y >= x + 2{
@@ -122,6 +143,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
                 m = m + 1
             }
         }else if  y > x && y == 11 {
+
             X.text = "B选手获胜"
             j = j + 1
             m = m + 1
@@ -147,9 +169,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
     @IBAction func end(_ sender: Any) {
         x = 0
         y = 0
+        c = 0
         A.text = "\(x)"
         B.text = "\(y)"
         X.text = ""
+        M.text = ""
         if i >= j{
             Y.text = "\(n)"
             Z.text = " VS "
